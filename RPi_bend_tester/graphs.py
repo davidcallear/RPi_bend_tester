2d50c8ba9f29aa2198c077825d90514e4aaebd24
David Callear <davidcallear.code@gmail.com>
2021-04-12 14:23:06 +0100

    Add docstrings to easy_plot and formal_plot functions

--- RPi_bend_tester/graphs.py
+++ RPi_bend_tester/graphs.py
@@ -2,13 +2,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def easy_plot(x, y, scat_fmt='kx', trend_fmt='k-', return_r2=True, show=True):
+    '''Plot scatter of `x` and `y` with best fit line.
+    Returns coefficients of linear rgeression line [and r squared value].
+    
+    Args:
+        x (tuple/list/numpy.ndarray): values of data points for the horizontal axis
+        y (tuple/list/numpy.ndarray): values of data points for the vertical axis
+    Kwargs:
+        scat_fmt (str): format for scatter plot, two characters as follows:
+            scat_fmt[0] = one character colour (sets `c` in matplotlib.pyplot.scatter)
+            scat_fmt[1] = marker type (sets `marker` in matplotlib.pyplot.scatter)
+        trend_fmt (str): format for trendline, passed directly to matplotlib.pyplot.plot
+        return_r2 (bool): whether to return the r squared value for the linear regression
+        show (bool): whether to show the graph (plt.show(block=False))
+    Returns:
+        tuple: Information about the linear regression:
+            return_r2==False: (gradient, intercept)
+            return_r2==True: ((gradient, intercept), r squared)
+    '''
     plt.scatter(x, y, c=scat_fmt[0], marker=scat_fmt[1])
     # Get coefficients of linear regression
     m_c = np.polyfit(x, y, 1)
     trend_func = np.poly1d(m_c)
     # calculate r squared for the linear regression
     if return_r2:
-        y = np.array(y)
+        if not isinstance(y, np.ndarray):
+            y = np.array(y)
         r2 = 1 - sum((yi-trend_func(xi))**2 for xi, yi in zip(x, y)) / np.sum((y-np.mean(y))**2)
     # plot trend line
     min_max_x = np.array((np.min(x), np.max(y)))
@@ -16,13 +35,28 @@ def easy_plot(x, y, scat_fmt='kx', trend_fmt='k-', return_r2=True, show=True):
     if show:
         plt.show(block=False)
     if return_r2:
-        return m_c, r2
+        return tuple(m_c), r2
     else:
-        return m_c
+        return tuple(m_c)
 
 def formal_plot(x, y, title, x_title, y_title, x_units, y_units, show=True):
+    '''Plots a `x` and `y` for a formal looking graph.
+    
+    Args:
+        x (tuple/list/numpy.ndarray): values of data points for the horizontal axis
+        y (tuple/list/numpy.ndarray): values of data points for the vertical axis
+        title (str): title of the graph
+        x_title (str): label for horizontal axis (excluding units)
+        y_title (str): label for vertical axis (excluding units)
+        x_units (tuple(tuple)): of (unit (str), exponent (int/str)) pairs for units of the horizontal axis
+        y_units (tuple(tuple)): of (unit (str), exponent (int/str)) pairs for units of the vertical axis
+    Kwargs:
+        show (bool): whether to show the graph (plt.show(block=False))
+    Returns:
+        tuple: Information about the linear regression: ((gradient, intercept), r squared)
+    '''
     def make_latex(units):
-        ''' Convert list of units and their exponents to latex (using superscripts).
+        '''Convert list of units and their exponents to latex (using superscripts).
         A unit without an exponent is assumed to be raised to the power of one.
         Exponents can be expressed as int or str.
         
