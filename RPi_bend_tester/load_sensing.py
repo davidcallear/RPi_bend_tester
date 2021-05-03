6dc4f243bccff049d754b7652ffa888bfea53901
David Callear <davidcallear.code@gmail.com>
2021-05-03 14:51:19 +0100

    Add explanatory comments

--- RPi_bend_tester/load_sensing.py
+++ RPi_bend_tester/load_sensing.py
@@ -11,18 +11,25 @@ from graphs import formal_plot
 REFERENCE_UNIT = 1
 SPACING = 5
 
+# allow for clean exit (through keyboard interrupt) from long measurement readings
 def cleanAndExit():
     print("Cleaning...")
     GPIO.cleanup()
     print("Bye!")
     sys.exit()
 
+# initialize HK711:
+# pin for dout (input pin) -> 5
+# pin for pd_sck (output pin) -> 6
 hx = HX711(5, 6)
 hx.set_reading_format("MSB", "MSB")
 
 hx.set_reference_unit(REFERENCE_UNIT)
 hx.reset()
 sleep(1)
+# tare the balance
+print('Now doing tare...')
+# find average value, and readings during and after pause
 try:
     tare_value, tare_pause_values, tare_values = hx.tare()
 except (KeyboardInterrupt, SystemExit):
@@ -30,9 +37,12 @@ except (KeyboardInterrupt, SystemExit):
 
 print("Tare done! Add weight now...")
 
+# wait for user to have added weight before proceeding
 input('Press enter once weight added')
 
 try:
+    # measure weight
+    # find average value, and readings during and after pause
     cal_value, cal_pause_values, cal_values = hx.read_pulse_average(times=15,
                                                                     duration=120,
                                                                     spacing=SPACING,
@@ -41,17 +51,20 @@ try:
 except (KeyboardInterrupt, SystemExit):
     cleanAndExit()
 
-# plot results for tare
+print('Value for tare:', tare_value)
+print('Value for measurement:', cal_value)
 
+# plot results for tare
+# plot values taken during initial pause (not used for value)
 formal_plot(tuple(range(0, len(tare_pause_values), SPACING)),
             tare_pause_values,
             )
-
+# plot vertical red line to seperate pause and value reading sections of graph
 plt.plot((len(tare_pause_values)-0.5,)*2,
          (0, max(tare_pause_values)),
          'r-'
 )
-
+# plot values taken during averaging for given value
 (gradient, intercept), r_squared = formal_plot(tuple(range(0, len(tare_values), SPACING)),
                                                tare_values,
                                                title='',
@@ -65,16 +78,16 @@ print('r squared =', r_squared)
 plt.show()
 
 # plot results for measurement
-
+# plot values taken during initial pause (not used for value)
 formal_plot(tuple(range(0, len(cal_pause_values), SPACING)),
             cal_pause_values,
             )
-
+# plot vertical red line to seperate pause and value reading sections of graph
 plt.plot((len(cal_pause_values)-0.5,)*2,
          (0, max(cal_pause_values)),
          'r-'
 )
-
+# plot values taken during averaging for given value
 (gradient, intercept), r_squared = formal_plot(tuple(range(0, len(cal_values), SPACING)),
                                                cal_values,
                                                )
