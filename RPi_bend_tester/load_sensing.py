be00cef4f5a54e8e4a9405acd893924a630c8f40
David Callear <davidcallear.code@gmail.com>
2021-05-03 14:38:56 +0100

    Plot measurement values too. Minor fixes

--- RPi_bend_tester/load_sensing.py
+++ RPi_bend_tester/load_sensing.py
@@ -9,7 +9,7 @@ from hk711 import HK711
 from graphs import formal_plot
 
 REFERENCE_UNIT = 1
-RUN_MINUTES = 5
+SPACING = 5
 
 def cleanAndExit():
     print("Cleaning...")
@@ -23,35 +23,63 @@ hx.set_reading_format("MSB", "MSB")
 hx.set_reference_unit(REFERENCE_UNIT)
 hx.reset()
 sleep(1)
-tare_value, tare_pause_values, tare_values = hx.tare()
+try:
+    tare_value, tare_pause_values, tare_values = hx.tare()
+except (KeyboardInterrupt, SystemExit):
+    cleanAndExit()
 
 print("Tare done! Add weight now...")
 
-input('Press enter to continue once calibration weight added')
+input('Press enter once weight added')
 
 try:
     cal_value, cal_pause_values, cal_values = hx.read_pulse_average(times=15,
                                                                     duration=120,
-                                                                    spacing=5,
+                                                                    spacing=SPACING,
                                                                     pause=60
                                                                     )
 except (KeyboardInterrupt, SystemExit):
     cleanAndExit()
 
+# plot results for tare
 
-formal_plot(tuple(range(len(tare_pause_values))),
+formal_plot(tuple(range(0, len(tare_pause_values), SPACING)),
             tare_pause_values,
             )
 
-plt.plot((len(tare_pause_values)-0.,)*2,
+plt.plot((len(tare_pause_values)-0.5,)*2,
          (0, max(tare_pause_values)),
          'r-'
 )
 
-(gradient, intercept), r_squared = formal_plot(tuple(range(len(tare_values))),
+(gradient, intercept), r_squared = formal_plot(tuple(range(0, len(tare_values), SPACING)),
                                                tare_values,
+                                               title='',
                                                )
 
+# Hopefully values are somewhat random, so r squared is close to zero
+print('Gradient = ', gradient)
+print('Intercept =', intercept)
+print('r squared =', r_squared)
+
+plt.show()
+
+# plot results for measurement
+
+formal_plot(tuple(range(0, len(cal_pause_values), SPACING)),
+            cal_pause_values,
+            )
+
+plt.plot((len(cal_pause_values)-0.5,)*2,
+         (0, max(cal_pause_values)),
+         'r-'
+)
+
+(gradient, intercept), r_squared = formal_plot(tuple(range(0, len(cal_values), SPACING)),
+                                               cal_values,
+                                               )
+
+# Hopefully values are somewhat random, so r squared is close to zero
 print('Gradient = ', gradient)
 print('Intercept =', intercept)
 print('r squared =', r_squared)
